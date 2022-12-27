6f7dc5706123d2e9d0038b369d188f1bcc0823f0
gasukaku <94212316+gasukaku@users.noreply.github.com>
2022-12-27 21:52:42 +0900

    Update header.js

--- common/header.js
+++ common/header.js
@@ -2,7 +2,6 @@ function header(){
     $.ajax({
         url: 'https://www.gasukaku.net/common/header.html',
         cache: false,
-        async: false,
         dataType: 'html',
         success: function(html){
             document.write(html);
