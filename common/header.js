2a9b7a49e41813d441b086bb614f2e0ac51d96a2
gasukaku <94212316+gasukaku@users.noreply.github.com>
2022-12-27 21:47:10 +0900

    Update header.js

--- common/header.js
+++ common/header.js
@@ -1,36 +1,11 @@
 function header(){
-var html = "";
-html += '<header>';
-html += '
- <div id="header2">
-<div id="header_yohaku2">
-  
-    </div>
-    <h3>
- <div class="h-menu">
-      <input id="h-menu_checkbox" class="h-menuCheckbox" type="checkbox">
-      <label class="h-menu_icon" for="h-menu_checkbox"><span class="hamburger-icon"></span></label>
-      <label id="h-menu_black" class="h-menuCheckbox" for="h-menu_checkbox"></label>
-      <div id="h-menu_content">
-        <ul>
-          <li><a href="../">ホーム</a></li>
-
-          <li><a href="../blog">ブログ</a>
-          </li>
-          
-           <li>
-             <a href="../links">相互リンク</a>
-          </li>
-          </ul></div>
-<div id="header_logp">
-  
-        
-        <div style="text-align: center"> 
-        <a href="#" style="color:white">Gasukaku</a>
-  <div id="header_yohaku">
-    </div></div></div></div></h3>   
-   
-    ';
-html += '</header>';
-document.write(html);
+    $.ajax({
+        url: '/common/tpl/header.html',
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            document.write(html);
+        }
+    });
 }
