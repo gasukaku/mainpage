e0319eb2e825876e1b05dee2ff9adef28298bf69
gasukaku <94212316+gasukaku@users.noreply.github.com>
2022-12-27 21:24:36 +0900

    Update header.js

--- common/header.js
+++ common/header.js
@@ -1,9 +1,33 @@
-function header(){
-	$.ajax({
-		url: "header.html",
-		cache: false,
-		success: function(html){
-			document.write(html);
-		}
-	});
-}
+window.onload = () => {
+  document.head.innerHTML += `<meta name="viewport" content="width=device-width, initial-scale=1">`
+  document.body.innerHTML = `
+   <!--ヘッダーはここから-->
+ <div id="header2">
+<div id="header_yohaku2">
+  
+    </div>
+    <h3>
+ <div class="h-menu">
+      <input id="h-menu_checkbox" class="h-menuCheckbox" type="checkbox">
+      <label class="h-menu_icon" for="h-menu_checkbox"><span class="hamburger-icon"></span></label>
+      <label id="h-menu_black" class="h-menuCheckbox" for="h-menu_checkbox"></label>
+      <div id="h-menu_content">
+        <ul>
+          <li><a href="../">ホーム</a></li>
+
+          <li><a href="../blog">ブログ</a>
+          </li>
+          
+           <li>
+             <a href="../links">相互リンク</a>
+          </li>
+          </ul></div>
+<div id="header_logp">
+  
+        
+        <div style="text-align: center"> 
+        <a href="#" style="color:white">Gasukaku</a>
+  <div id="header_yohaku">
+    </div></div></div></div></h3>   
+   
+    <!--ヘッダーここまで-->
