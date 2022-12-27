b2ddd29ac2d5b2bbad759887b31249a944ba0d89
gasukaku <94212316+gasukaku@users.noreply.github.com>
2022-12-27 22:18:14 +0900

    Update header.js

--- common/header.js
+++ common/header.js
@@ -2,6 +2,7 @@ function writeHeader(rootDir){
     $.ajax({
         url: rootDir + "/common/header.html",
         cache: false,
+        async: false,
         success: function(html){
 
             html = html.replace(/\{\$root\}/g, rootDir);
