62fc881caa4c75d4efb4f1234ac28c39c264c033
gasukaku <94212316+gasukaku@users.noreply.github.com>
2022-12-27 21:07:20 +0900

    Update header.js

--- common/header.js
+++ common/header.js
@@ -1,1 +1,11 @@
+function writeHeader(rootDir){
+    $.ajax({
+        url: rootDir + "header.html", 
+        cache: false, //キャッシュを利用するか（お好みで）
+         success: function(html){
 
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
