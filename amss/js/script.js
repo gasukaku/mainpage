19dedc9bf3338c5f40d8a5beb48b5f3b9ddf7c39
gasukaku <94212316+gasukaku@users.noreply.github.com>
2023-03-12 21:56:21 +0900

    Update script.js

--- amss/js/script.js
+++ amss/js/script.js
@@ -1,19 +1,6 @@
 function writeHeader(rootDir){
   $.ajax({
-       url : "header.html",
-        cache: false,
-        async: false,
-        success: function(html){
-
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
-}
-
-function writefooter(rootDir){
-  $.ajax({
-       url : "footer.html",
+        url: "/header.html",
         cache: false,
         async: false,
         success: function(html){
