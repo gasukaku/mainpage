c217539de87bd4af6d78e4e06b67a1b2bfcf5866
gasukaku <94212316+gasukaku@users.noreply.github.com>
2023-04-09 21:24:56 +0900

    Update script.js

--- amss/js/script.js
+++ amss/js/script.js
@@ -10,3 +10,16 @@ function writeHeader(rootDir){
         }
     });
 }
+
+function writeFooter(rootDir){
+  $.ajax({
+        url: "/amss/js/footer.html",
+        cache: false,
+        async: false,
+        success: function(html){
+
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
