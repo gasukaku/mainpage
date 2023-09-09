2667489f8443c43eb80d4ef0beaa2ebbb3b80b1d
gasukaku <94212316+gasukaku@users.noreply.github.com>
2023-09-10 00:44:33 +0900

    Update qbox.js

--- qbox.js
+++ qbox.js
@@ -1,29 +1,32 @@
-const questionInput = document.getElementById('question');
-const message = document.getElementById('message');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('question-form');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const name = nameInput.value;
-  const email = emailInput.value;
-  const question = questionInput.value;
+    // Google Apps Scriptの公開ウェブアプリケーションURL
+    const gasUrl = 'https://script.google.com/macros/s/your_script_id/exec';
 
-  const payload = {
-    'entry.2468101214': question
-  };
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const question = document.getElementById('question').value;
 
-  const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSf7kBLi6T5fE79hYuBCDsxH1oy3_RoAHmKC_lZPdnpquaWJKA/formResponse';
-  const options = {
-    'method': 'post',
-    'payload': payload
-  };
-
-  message.textContent = '送信中...';
-  fetch(formUrl, options)
-    .then(() => {
-      message.textContent = '送信しました！';
-      form.reset();
-    })
-    .catch(() => {
-      message.textContent = '送信に失敗しました。しばらくしてから再度お試しください。';
+        // Google Apps Scriptにデータを送信
+        fetch(gasUrl, {
+            method: 'POST',
+            body: JSON.stringify({ question }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                alert('質問が送信されました！');
+                document.getElementById('question').value = ''; // テキストボックスをクリア
+            } else {
+                alert('質問の送信に失敗しました。');
+            }
+        })
+        .catch(error => {
+            console.error('エラー:', error);
+            alert('エラーが発生しました。');
+        });
     });
 });
